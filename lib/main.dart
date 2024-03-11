c09ef827ae44301078cd7e35396af37085106abb
Maksim Lin <maks@manichord.com>
2024-03-11 16:44:38 +1100

    basic import from hardcoded location

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:enough_mail/imap.dart';
 import 'package:flutter/material.dart';
+import 'package:path/path.dart' as p;
 
 void main() {
   runApp(const MyApp());
@@ -12,43 +15,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Mail Notes',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Mail Notes'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -101,6 +79,21 @@ class _MyHomePageState extends State<MyHomePage> {
     }
   }
 
+  Future<void> _importNotes(Directory importPath) async {
+    final entriesStream = importPath.list();
+    entriesStream.forEach((el) async {
+      // print("file: ${el.path}");
+      if (el is File) {
+        final name = p.basename(el.path);
+        if (name.endsWith('.md') || name.endsWith(".txt")) {
+          final noteText = await el.readAsString();
+          final n = Note(name, noteText);
+          print("note: $n");
+        }
+      }
+    });
+  }
+
   Future<MimeMessage> _newNote(String title, String text) async {
     final builder = MessageBuilder();
     builder.addTextPlain(text);
@@ -145,11 +138,33 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
+        actions: [
+          IconButton(
+            icon: const Icon(
+              Icons.upload,
+              color: Colors.white,
+            ),
+            onPressed: () {
+              print("import...");
+              _importNotes(Directory("/temp"));
+            },
+          ),
+          IconButton(
+            icon: const Icon(
+              Icons.settings,
+              color: Colors.white,
+            ),
+            onPressed: () {
+               throw UnimplementedError("settings not done yet");
+            },
+          ),
+          
+        ],
       ),
       body: Center(
         child: Row(
           children: [
-            Container(
+            SizedBox(
               height: 300,
               width: 200,
               child: ListView(
@@ -192,7 +207,6 @@ class _NoteEditorState extends State<NoteEditor> {
   void initState() {
     super.initState();
     _textController.text = widget.note.body;
-    print("body: ${widget.note.body.substring(0, 20)}");
   }
 
   @override
@@ -207,8 +221,8 @@ class _NoteEditorState extends State<NoteEditor> {
         SizedBox(
           width: 220,
           child: TextField(
-            // controller: _textController,
-          ),
+              // controller: _textController,
+              ),
         ),
       ],
     );
@@ -220,4 +234,9 @@ class Note {
   final String body;
 
   Note(this.title, this.body);
+
+  @override
+  String toString() {
+    return "$title\n\n$body";
+  }
 }
